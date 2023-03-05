32830f7ff4d9c3be68bd2a2fa826a8ee733c6e01
mirsona9 <mcakani19@epoka.edu.al>
2023-03-05 10:14:45 +0100

    Initial commit

---
+++ src/test/java/com/carrentapp/CarRentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
