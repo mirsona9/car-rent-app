5a158a5482f11b94c38a2086adf7a8b72a41807c
mirsona9 <mcakani19@epoka.edu.al>
2023-03-12 14:28:14 +0100

    Adding the detail page

--- src/main/java/com/carrentapp/controllers/CarsController.java
+++ src/main/java/com/carrentapp/controllers/CarsController.java
@@ -3,6 +3,8 @@ package com.carrentapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CarsController {
@@ -11,5 +13,17 @@ public class CarsController {
         map.addAttribute("menu", "cars");
         return "car/index";
     }
+
+    @GetMapping("/cars/detail")
+    public String detail(ModelMap map) {
+        map.addAttribute("menu", "detail");
+        return "detail/index";
+    }
+    @PostMapping("/cars/detail/rent")
+    public String rent(ModelMap map) {
+        map.addAttribute("menu", "detail");
+        return "detail/index";
+    }
+
 }
 
